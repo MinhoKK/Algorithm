018fa871a170051703da2ed897dafc899e14afbd
MinhoKK <koho1047@naver.com>
2024-07-15 15:21:13 +0900

    백준 1520 내리막길

--- GraphTraversal/BJ_1520_내리막길.java
+++ GraphTraversal/BJ_1520_내리막길.java
@@ -7,26 +7,39 @@ import static java.lang.Integer.parseInt;
 
 public class BJ_1520_내리막길 {
     /**
+     * @author 고민호
+     * 백준 1520 내리막기
+     * 난이도 G3
+     * 결과 : 통과      메모리 : 37,112KB      시간 : 396ms
      *
      * 풀이방법
      * 단순 BFS로 경우의 수를 찾으면 메모리 초과 발생..
-     * visit한 곳을 다시 방문하면 count 증가하고 그 분기는 삭제로 해결해보기
+     * 1. 높이를 내림차순으로 정렬 -> 높이가 낮은 좌표는 이미 queue에 추가되어 있는 상태에서 높은 높이를 통한 경우의 수를 구해서 낮은 좌표까지의 경우의 수와 더하기 위함
+     * 2. visit 배열의 값은 해당 좌표까지의 경우의 수
      */
 
-    static class Node {
+    static class Node implements Comparable<Node>{
         int r;
         int c;
+        int value;
 
-        public Node(int r, int c) {
+        public Node(int r, int c, int value) {
             this.r = r;
             this.c = c;
+            this.value = value;
         }
+
+        // value 내림차순
+        @Override
+        public int compareTo(Node o){
+            return o.value - this.value;
+        }
+
     }
 
     static int M, N;    // 세로, 가로
     static int[][] map;
     static int[][] visit;
-    static int result;
     static int[] dr = {-1, 1, 0, 0};
     static int[] dc = {0, 0, -1, 1};
 
@@ -48,38 +61,34 @@ public class BJ_1520_내리막길 {
         }
 
         bfs();
-
-
-        System.out.println(result);
-
+        System.out.println(visit[M-1][N-1]);
     }
 
 
     static void bfs() {
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(new Node(0, 0));
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(0, 0, map[0][0]));
+        visit[0][0] = 1;
 
-        while (!queue.isEmpty()) {
-            Node out = queue.poll();
-            if (out.r == M - 1 && out.c == N - 1) {
-                result++;
-            }
-
-            for (int d = 0; d < 4; d++) {
+        while(!pq.isEmpty()){
+            Node out = pq.poll();
+            for(int d=0; d<4; d++){
                 int nr = out.r + dr[d];
                 int nc = out.c + dc[d];
-                if (nr >= 0 && nr < M && nc >= 0 && nc < N && map[out.r][out.c] > map[nr][nc]) {
+                if(nr >= 0 && nr < M && nc >= 0 && nc < N && map[out.r][out.c] > map[nr][nc]){  // 다음 좌표의 값이 현재 좌표의 값보다 작은 경우
+                    // 처음 방문한 경우
                     if(visit[nr][nc] == 0){
-                        visit[nr][nc] = 1;
-                        queue.add(new Node(nr, nc));
-                    } else {
-                        result++;
+                        visit[nr][nc] = visit[out.r][out.c];    // visit[x][y] 는 x,y까지 올수있는 경우의 수로 처음 방문이라면 현재 좌표까지의 방문의 수와 동일
+                        pq.add(new Node(nr, nc, map[nr][nc]));  // queue 추가
+                    // 방문한적 있는 경우    
+                    } else if(visit[nr][nc] > 0){
+                        visit[nr][nc] += visit[out.r][out.c];   // 다음 방문할 곳까지 가는 경우의 수 = 현재 지점까지의 경우의 수 + 다음 좌표까지의 경우의 수
+                                                                // 그리고 visit > 0 이라면 이미 queue에 들어있기에 add하지 않음
                     }
-
-
 
                 }
             }
         }
+
     }
 }
