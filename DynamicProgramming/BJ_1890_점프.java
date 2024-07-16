23cab17271dc8366e0bd37dc9766c0a0c7db7ce0
MinhoKK <koho1047@naver.com>
2024-07-16 17:39:09 +0900

    백준 1890 점프 해결

--- DynamicProgramming/BJ_1890_점프.java
+++ DynamicProgramming/BJ_1890_점프.java
@@ -5,22 +5,20 @@ import java.util.*;
 import static java.lang.Integer.parseInt;
 
 public class BJ_1890_점프 {
-
-    static class Node {
-        int r;
-        int c;
-        int value;
-
-        public Node(int r, int c, int value) {
-            this.r = r;
-            this.c = c;
-            this.value = value;
-        }
-    }
+    /**
+     * @author 고민호
+     * 백준_1890_점프
+     * 난이도 S1
+     * 결과 : 통과      메모리 : 14,292KB      시간 : 104ms
+     *
+     * 풀이방법
+     * 1. 2차원 배열을 순차적으로 순회하며 현재 좌표에서 이동 가능한 좌표 찾기
+     * 2. 이동가능한 좌표의 방문 수 = 현재 좌표의 방문 수 + 이동 가능한 좌표의 방문 수
+     */
 
     static int N;
     static int[][]  map;
-    static int[][] visit;
+    static long[][] visit;
     static int[] dr = {1, 0};  // 하, 우
     static int[] dc = {0, 1};
 
@@ -31,7 +29,7 @@ public class BJ_1890_점프 {
         N = parseInt(br.readLine());
 
         map = new int[N][N];
-        visit = new int[N][N];
+        visit = new long[N][N];
 
         for(int i=0; i<N; i++){
             st = new StringTokenizer(br.readLine());
@@ -40,32 +38,32 @@ public class BJ_1890_점프 {
             }
         }
 
-        bfs();
+        visit[0][0] = 1;    // 시작 좌표 1로 초기화 -> 방문한 것으로 표기하기 위해
 
-        System.out.println(visit[N-1][N-1]);
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++){
 
-    }
-
-    static void bfs(){
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(new Node(0, 0, map[0][0]));
-        visit[0][0] = 1;
-
-        while(!queue.isEmpty()){
-            Node out = queue.poll();
-            if(out.r == N-1 && out.c == N-1){
-                continue;
-            }
-            for(int d=0; d<2; d++){
-                int nr = out.r + (dr[d]*out.value);
-                int nc = out.c + (dc[d]*out.value);
-
-                if(nr >= 0 && nr < N && nc >= 0 && nc < N){
-                    visit[nr][nc]++;
-                    queue.add(new Node(nr, nc, map[nr][nc]));
+                // 종료 지점에 도착한 경우
+                if(map[i][j] == 0){
+                    System.out.println(visit[N-1][N-1]);
+                    return;
                 }
-            }
 
+                // 방문을 한 경우
+                if(visit[i][j] > 0){
+                    // 우측, 하단 방향으로 이동 가능한지 확인하기
+                    for(int d=0; d<2; d++){
+                        int nr = i + (dr[d]*map[i][j]);
+                        int nc = j + (dc[d]*map[i][j]);
+
+                        // 범위 내로 이동이 가능한 경우라면
+                        if(nr >= 0 && nr < N && nc >= 0 && nc < N){
+                            visit[nr][nc] += visit[i][j];   // 이동 가능 좌표까지의 경우의 수 = 이동 가능 좌표까지의 경우의 수 + 현재 좌표까지의 경우의 수
+                        }
+                    }
+                }
+
+            }
         }
     }
 }
